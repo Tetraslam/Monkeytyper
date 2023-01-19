f581e6fd76511395a4c3cc9a13b2c964f646cc83
Tetraslam <bhowmickshresht@gmail.com>
2023-01-19 13:52:06 +0530

    t

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import pyautogui
 
 
-
 def type_file(file_path):
     with open(file_path, 'r') as f:
         for char in f.read():
             pyautogui.typewrite(char)
 
-file_path = "typetext.txt"
+file_path = "typetext.txt" # Add the text shown on screen here
 type_file(file_path)
